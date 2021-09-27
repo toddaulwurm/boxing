3c072562fa4ba5771b83311bd2fad021d6d6ea7d
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-09-26 21:22:16 -0500

    summ

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace multi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<object> boxy = new List<object>();
+            boxy.Add(7);
+            boxy.Add(28);
+            boxy.Add(-1);
+            boxy.Add(true);
+            boxy.Add("chair");
+            int sum = 0;
+            foreach(object item in boxy)
+            {
+                Console.WriteLine(item);
+                if(item is int)
+                {
+                    sum+= (int)item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
